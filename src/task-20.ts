b416c4349ce0c11cb9db07fe2b05493fbb7dc43d
Oleksandr Palilov <opalilovit@gmail.com>
2025-05-13 21:40:36 +0300

    finish work task 20

--- src/task-20.ts
+++ src/task-20.ts
@@ -1,17 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
 interface Post {
-    id: number;
-    title: string;
-    body: string;
+  id: number;
+  title: string;
+  body: string;
 }
-
-const fetchPosts = async ():Promise<Post[]> => {
-  const response = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
+interface User {
+  body: string;
+  id: number;
+  title: string;
+}
+const fetchPosts = async (): Promise<Post[]> => {
+  const response = await axios.get<Post[]>(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
   return response.data;
 };
 
-const getUsers = async () => {
-    const users = await fetchPosts();
-    console.log(users)
-};
+const users = await fetchPosts();
+
+function logThreePosts(users: User[]): User[] {
+  return users.filter(({ body, title, id }: User) =>
+    id < 4 ? { body, title } : ''
+  );
+}
+console.log(logThreePosts(users));
